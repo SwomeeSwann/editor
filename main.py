21c45491bf4ef267ba8972fa2977ecc1d94426c7
Majd Alkhdr <totallynotswann@gmail.com>
2024-05-09 07:58:53 -0500

    Pathched bugs

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((1280, 720))
 run = True
 
-names = ["Dale", "Mahogany", "Bruh", "Cool"]
+names = ["Dale", "Mahogany", "Bruh", "Cool", "Bob", "Joe"]
 circles = []
 hovered = []
 
@@ -25,10 +25,21 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            for circle in circles:
+                if abs(mouse_pos[0] - circle.pos.x) < 25 and abs(mouse_pos[1] - circle.pos.y) < 25:
+                    circles.pop(circles.index(circle))
+                    circles = circles[:1] + circles[1:]
+                    if circle in hovered:
+                        hovered.pop(hovered.index(circle))
+                        hovered = hovered[:1] + hovered[1:]
+
+
+
     mouse_pos = pygame.mouse.get_pos()
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_SPACE]:
+    if keys[pygame.K_SPACE]: 
         circle = Circle(pygame.Vector2(random.randint(850, 1280), random.randint(0, 720)), "blue", names[random.randint(0, len(names) - 1)])
         circles.append(circle)
             
@@ -37,20 +48,21 @@ while run:
     pygame.draw.rect(screen, "gray", pygame.Rect(0, 0, 800, 720))
     
     for circle in circles:
-        pygame.draw.circle(screen, circle.color, circle.pos, 40)
         if abs(mouse_pos[0] - circle.pos.x) < 25 and abs(mouse_pos[1] - circle.pos.y) < 25:
-            
+
+            pygame.draw.circle(screen, "white", circle.pos, 45)
+
             text_surface = font.render(str(circle.pos) + ", " + str(circle.name), False, (255, 255, 255))
-            
             if not circle in hovered:
                 hovered.append(circle)
             screen.blit(text_surface, (25, 25 + (75 * hovered.index(circle))))
         else:
             if circle in hovered:
                 hovered.pop(hovered.index(circle))
+        
+        pygame.draw.circle(screen, circle.color, circle.pos, 40)
     
     
-
                 
     
     pygame.display.flip()
